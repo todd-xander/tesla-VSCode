868b08900fae64e5298f2494f37303b40c248622
zhangtao <zhangtao@sensetime.com>
2022-05-06 20:14:12 +0800

    improve image fetch url generation

--- media/tesla/sidebar.js
+++ media/tesla/sidebar.js
@@ -38,8 +38,8 @@ function toggleAutoRenew(ev) {
 
 function buildBasicInfo(infoView, vv) {
   let loc = "";
-  let odo = "";
   let ts = "";
+  let drive = "";
   if (vv.state !== "asleep") {
     let tip = "Location: ";
     let lat = vv.drive_state.corrected_latitude;
@@ -54,24 +54,41 @@ function buildBasicInfo(infoView, vv) {
     } else {
       tip += `,${lng * -1}W`;
     }
-    loc = `<a class='locIcon' title='${tip}' href='https://www.google.com/maps/search/?api=1&query=${vv.drive_state.corrected_latitude},${vv.drive_state.corrected_longitude}'>
+    loc = `<a class='locIcon' title='${tip}' href='${vv.location}'>
             <span class="material-symbols-outlined">explore</span>
           </a>`;
-    odo = `<vscode-badge class='odometer'>
-            ${Math.floor(vv.vehicle_state.odometer + 0.5)}mi
-          </vscode-badge>`;
-    if (vv.gui_settings.gui_distance_units === "km/hr") {
-      odo = `<vscode-badge class='odometer'>
-              ${Math.floor(vv.vehicle_state.odometer * 1.609344 + 0.5)}km
-            </vscode-badge>`;
-    }
     ts = `<span class='timestamp'>
             ${new Date(vv.vehicle_state.timestamp).toLocaleString()}
           </span>`;
+
+    let shift = vv.drive_state.shift_state || "P";
+
+    drive = `<div class='drive-info'>`;
+    drive += ["P", "R", "N", "D"]
+      .map((v, i, arr) => {
+        if (shift === v) {
+          return `<span class='shift-state enable'>${v}</span>`;
+        }
+        return `<span class='shift-state'>${v}</span>`;
+      })
+      .join("");
+
+    if (shift === "D" || shift === "R") {
+      if (vv.gui_settings.gui_distance_units === "km/hr") {
+        drive += `<span>
+              ${Math.floor(vv.drive_state.speed * 1.609344 + 0.5)}km/h
+            </span>`;
+      } else {
+        drive += `<span>${vv.drive_state.speed}mi/h</span>`;
+      }
+    }
+
+    drive += `</div>`;
   }
 
   let basicInfo = `
   <h1 class='banner'>${vv.display_name}${loc}</h1>
+  ${drive}
   ${ts}
   <img class='car-img' src='${vv.image}'/>`;
 
@@ -122,20 +139,21 @@ function buildBasicInfo(infoView, vv) {
 
 function buildShortcutView(shortcutView, vv) {
   if (vv.state == "asleep") {
+    shortcutView.innerHTML = "";
     return;
   }
   let lockBtn =
-    "<vscode-button class='shortcut' title='Unlocked'><span class='material-symbols-outlined'>lock_open</span></vscode-button>";
+    "<vscode-button class='shortcut' title='Unlocked'><span class='material-symbols-outlined'>key</span></vscode-button>";
   if (vv.vehicle_state.locked) {
     lockBtn =
-      "<vscode-button class='shortcut' appearance='secondary' title='Locked'><span class='material-symbols-outlined'>lock</span></vscode-button>";
+      "<vscode-button class='shortcut' appearance='secondary' title='Locked'><span class='material-symbols-outlined'>key_off</span></vscode-button>";
   }
 
   let startupBtn =
-    "<vscode-button class='shortcut' appearance='secondary' title='Standby'><span class='material-symbols-outlined'>vpn_key</span></vscode-button>";
+    "<vscode-button class='shortcut' appearance='secondary' title='Standby'><span class='material-symbols-outlined'>settings_remote</span></vscode-button>";
   if (vv.vehicle_state.remote_start) {
     startupBtn =
-      "<vscode-button class='shortcut' title='Startup'><span class='material-symbols-outlined'>vpn_key</span></vscode-button>";
+      "<vscode-button class='shortcut' title='Startup'><span class='material-symbols-outlined'>settings_remote</span></vscode-button>";
   }
 
   let climateBtn =
@@ -146,14 +164,14 @@ function buildShortcutView(shortcutView, vv) {
   }
 
   let chargeBtn =
-    "<vscode-button class='shortcut' appearance='secondary' title='Charger Disconnected'><span class='material-symbols-outlined'>outlet</span></vscode-button>";
+    "<vscode-button class='shortcut' appearance='secondary' title='Charger Disconnected'><span class='material-symbols-outlined'>power_off</span></vscode-button>";
   if (
     vv.charge_state.charge_port_door_open &&
     vv.charge_state.charge_port_latch === "Engaged"
   ) {
     if (vv.charge_state.charging_state === "Stopped") {
       chargeBtn =
-        "<vscode-button class='shortcut' appearance='secondary' title='Charge Stopped'><span class='material-symbols-outlined'>electrical_services</span></vscode-button>";
+        "<vscode-button class='shortcut' title='Chager Connected'><span class='material-symbols-outlined'>power</span></vscode-button>";
     } else if (vv.charge_state.charging_state === "Complete") {
       chargeBtn =
         "<vscode-button class='shortcut' title='Charge Complete'><span class='material-symbols-outlined'>bolt</span></vscode-button>";
@@ -168,10 +186,10 @@ function buildShortcutView(shortcutView, vv) {
   let falshBtn =
     "<vscode-button class='shortcut' appearance='secondary' title='Flash Headlights'><span class='material-symbols-outlined'>online_prediction</span></vscode-button>";
   let defrost =
-    "<vscode-button class='shortcut' appearance='secondary' title='Defrost'><span class='material-symbols-outlined'>mode_cool_off</span></vscode-button>";
+    "<vscode-button class='shortcut' appearance='secondary' title='Defrost Off'><span class='material-symbols-outlined'>astrophotography_off</span></vscode-button>";
   if (vv.climate_state.defrost_mode == 1) {
     defrost =
-      "<vscode-button class='shortcut' title='Defrost'><span class='material-symbols-outlined'>mode_cool</span></vscode-button>";
+      "<vscode-button class='shortcut' title='Defrost On'><span class='material-symbols-outlined'>auto_awesome</span></vscode-button>";
   }
 
   let frunk =
@@ -205,13 +223,14 @@ function buildShortcutView(shortcutView, vv) {
 
 function buildControlPanels(controlView, vv) {
   if (vv.state == "asleep") {
+    controlView.innerHTML = "";
     return;
   }
 
   controlView.innerHTML = `
     <vscode-panels>
     <vscode-panel-tab title='Aaction'><span class="material-symbols-outlined">directions_car</span></vscode-panel-tab>
-    <vscode-panel-tab title='Climate'><span class="material-symbols-outlined">airware</span></vscode-panel-tab>
+    <vscode-panel-tab title='Climate'><span class="material-symbols-outlined">ac_unit</span></vscode-panel-tab>
     <vscode-panel-tab title='Charge'><span class="material-symbols-outlined">electrical_services</span></vscode-panel-tab>
     <vscode-panel-tab title='Security'><span class="material-symbols-outlined">security</span></vscode-panel-tab>
     <vscode-panel-tab title='JSON Response'><span class="material-symbols-outlined">data_object</span></vscode-panel-tab>
@@ -229,8 +248,10 @@ function buildControlPanels(controlView, vv) {
 
 function buildFooter(footerView, vv) {
   if (vv.state == "asleep") {
+    footerView.innerHTML = "";
     return;
   }
+
   let car_type = vv.vehicle_config.car_type;
   car_type = car_type.toUpperCase();
 
@@ -278,15 +299,9 @@ function buildFooter(footerView, vv) {
   `;
 }
 
-window.addEventListener("message", (event) => {
-  const message = event.data;
-  switch (message.command) {
-    case "vehicle": {
-      var vv = message.data;
-      var view = document.getElementById(vv.id_s);
-      stopPollingVehicleState(vv.id_s);
-
-      view.innerHTML = `<vscode-data-grid class="view-grid" generate-header="none" aria-label="No Header">
+function buildFramework(view, data) {
+  stopPollingVehicleState(data.id_s);
+  view.innerHTML = `<vscode-data-grid class="view-grid" generate-header="none" aria-label="No Header">
           <vscode-data-grid-row>
             <vscode-data-grid-cell grid-column="1" class="info-view"></vscode-data-grid-cell>
           </vscode-data-grid-row>
@@ -299,44 +314,44 @@ window.addEventListener("message", (event) => {
           <vscode-data-grid-row class="footer-grid">
           </vscode-data-grid-row>
         </vscode-data-grid>`;
-      if (vv.state !== "asleep") {
-        view.innerHTML += `<div title="Auto Renew"
-                                data-vid="${vv.id_s}"
-                                class="auto-renew"
-                                onclick='toggleAutoRenew(event)'>
-                            <span class="material-symbols-outlined">
-                              sync_disabled
-                            </span>
-                          </div>`;
-      }
+  if (data.state !== "asleep") {
+    view.innerHTML += `<div title="Auto Renew"
+                            data-vid="${data.id_s}"
+                            class="auto-renew"
+                            onclick='toggleAutoRenew(event)'>
+                         <span class="material-symbols-outlined">
+                           sync_disabled
+                         </span>
+                       </div>`;
+  }
+}
 
-      var infoView = view.querySelector(".info-view");
-      buildBasicInfo(infoView, vv);
+function buildContent(view, data) {
+  var infoView = view.querySelector(".info-view");
+  buildBasicInfo(infoView, data);
 
-      var shortcutView = view.querySelector(".shortcut-view");
-      buildShortcutView(shortcutView, vv);
+  var shortcutView = view.querySelector(".shortcut-view");
+  buildShortcutView(shortcutView, data);
 
-      var controlView = view.querySelector(".control-view");
-      buildControlPanels(controlView, vv);
+  var controlView = view.querySelector(".control-view");
+  buildControlPanels(controlView, data);
 
-      var footerView = view.querySelector(".footer-grid");
-      buildFooter(footerView, vv);
+  var footerView = view.querySelector(".footer-grid");
+  buildFooter(footerView, data);
+}
 
+window.addEventListener("message", (event) => {
+  const message = event.data;
+  switch (message.command) {
+    case "vehicle": {
+      var view = document.getElementById(message.data.id_s);
+      buildFramework(view, message.data);
+      buildContent(view, message.data);
       break;
     }
     case "update": {
-      var vv = message.data;
-      var view = document.getElementById(vv.id_s);
-
-      var infoView = view.querySelector(".info-view");
-      buildBasicInfo(infoView, vv);
-
-      var shortcutView = view.querySelector(".shortcut-view");
-      buildShortcutView(shortcutView, vv);
-
-      var footerView = view.querySelector(".footer-grid");
-      buildFooter(footerView, vv);
-
+      var view = document.getElementById(message.data.id_s);
+      buildContent(view, message.data);
       break;
     }
   }
--- src/tesla/tesla.ts
+++ src/tesla/tesla.ts
@@ -59,43 +59,243 @@ export class TeslaSidebarProvider implements vscode.WebviewViewProvider {
     );
   }
 
-  private async getVehicleInfo(vid: string, vehicle: tjs.Vehicle): Promise<Object> {
+  private optionCodesToCarType(codes: String[]): String[] {
+    for (var v of codes) {
+      if (v === 'MDLS' || v === 'MS03' || v === 'MS04' || v === 'MS05') { return ['ms', 'Model S']; }
+      if (v === 'MS06') { return ['mx', 'Plaid 2021']; }
+      if (v === 'MDLX') { return ['mx', 'Model X']; }
+      if (v === 'MDL3') { return ['m3', 'Model 3']; }
+      if (v === 'MDLY') { return ['my', 'Model Y']; }
+    }
+    return ["", ""];
+  }
+
+  private optionCodesToWheelType(codes: String[]): String[] {
+    for (var v of codes) {
+      if (v === 'W32P') { return [v, '20" Performance Wheels']; }
+      if (v === 'W32D') { return [v, '20" Gray Performance Wheels']; }
+      if (v === 'W33D') { return [v, '20" Black Performance Wheels 2021']; }
+      if (v === 'W38B') { return [v, '18" Aero Wheels']; }
+      if (v === 'W39B') { return [v, '19" Sport Wheels']; }
+      if (v === 'W40B') { return [v, '18" Wheels']; }
+      if (v === 'W41B') { return [v, '19" Wheels']; }
+      if (v === 'WS90') { return [v, '19" Tempest Wheels']; }
+      if (v === 'WT19') { return [v, '19" Wheels']; }
+      if (v === 'WS10') { return [v, '21" Arachnid Wheels']; }
+      if (v === 'WT20') { return [v, '20" Silver Slipstream Wheels']; }
+      if (v === 'WT22') { return [v, '22" Silver Turbine Wheels']; }
+      if (v === 'WTAB') { return [v, '21" Black Arachnid Wheels']; }
+      if (v === 'WTAS') { return [v, '19" Silver Slipstream Wheels']; }
+      if (v === 'WTD2') { return [v, '19" Sonic Carbon Slipstream Wheels (8.5 in)']; }
+      if (v === 'WTDS') { return [v, '19" Grey Slipstream Wheels']; }
+      if (v === 'WTNN') { return [v, '20" Nokian Winter Tires (non-studded)']; }
+      if (v === 'WTNS') { return [v, '20" Nokian Winter Tires (studded)']; }
+      if (v === 'WTP2') { return [v, '20" Pirelli Winter Tires']; }
+      if (v === 'WTSC') { return [v, '20" Sonic Carbon Wheels']; }
+      if (v === 'WTSD') { return [v, '20" Two-Tone Slipstream Wheels']; }
+      if (v === 'WTSG') { return [v, '21" Turbine Wheels']; }
+      if (v === 'WTSP') { return [v, '21" Turbine Wheels']; }
+      if (v === 'WTSS') { return [v, '21" Turbine Wheels']; }
+      if (v === 'WTHX') { return [v, '20" Turbine Wheels']; }
+      if (v === 'WTTG') { return [v, '19" Cyclone Wheels']; }
+      if (v === 'WTTB') { return [v, '19" Cyclone Wheels']; }
+      if (v === 'WTTC') { return [v, '21" Sonic Carbon Twin Turbine Wheels']; }
+      if (v === 'WTUT') { return [v, '22" Onyx Black Wheels']; }
+      if (v === 'WTW2') { return [v, '19" Nokian Winter Wheel Set']; }
+      if (v === 'WTW3') { return [v, '19" Pirelli Winter Wheel Set']; }
+      if (v === 'WTW4') { return [v, '19" Winter Tire Set']; }
+      if (v === 'WTW5') { return [v, '21" Winter Tire Set']; }
+      if (v === 'WTW6') { return [v, '19" Nokian Winter Tires (studded)']; }
+      if (v === 'WTW7') { return [v, '19" Nokian Winter Tires (non-studded)']; }
+      if (v === 'WTW8') { return [v, '19" Pirelli Winter Tires']; }
+      if (v === 'WTX1') { return [v, '19" Michelin Primacy Tire Upgrade']; }
+      if (v === 'WX00') { return [v, '20" Cyberstream Wheels']; }
+      if (v === 'WX20') { return [v, '22" Turbine Wheels']; }
+      if (v === 'WXNN') { return [v, '20" Nokian Winter Tires (non-studded)']; }
+      if (v === 'WXNS') { return [v, '20" Nokian Winter Tires (studded)']; }
+      if (v === 'WXP2') { return [v, '20" Pirelli Winter Tires']; }
+      if (v === 'WXW2') { return [v, '19" Wheels with Nokian Winter Tyres']; }
+      if (v === 'WXW3') { return [v, '19" Wheels with Pirelli Winter Tyres']; }
+      if (v === 'WXW4') { return [v, '19" Winter Tire Set']; }
+      if (v === 'WXW5') { return [v, '21" Winter Tire Set']; }
+      if (v === 'WXW6') { return [v, '19" Nokian Winter Tires (studded)']; }
+      if (v === 'WXW7') { return [v, '19" Nokian Winter Tires (non-studded)']; }
+      if (v === 'WXW8') { return [v, '19" Pirelli Winter Tires']; }
+      if (v === 'WY0S') { return [v, '20" Induction']; }
+      if (v === 'WY18B') { return [v, '18" Aero Wheels']; }
+      if (v === 'WY1S') { return [v, '21" Uberturbine']; }
+      if (v === 'WY9S') { return [v, '19" Apollo']; }
+      if (v === 'WY19B') { return [v, '19" Sport Wheels']; }
+      if (v === 'WY20P') { return [v, '20" Performance Wheels']; }
+    }
+    return ["", ""];
+  }
+
+  private optionCodesToPaintColor(codes: String[]): String[] {
+    for (var v of codes) {
+      if (v === 'PBCW') { return [v, ' Solid White Color']; }
+      if (v === 'PBSB') { return [v, ' Solid Black Color']; }
+      if (v === 'PMAB') { return [v, ' Anza Brown Metallic Color']; }
+      if (v === 'PMBL') { return [v, ' Obsidian Black Multi-Coat Color']; }
+      if (v === 'PMMB') { return [v, ' Monterey Blue Metallic Color']; }
+      if (v === 'PMNG') { return [v, ' Midnight Silver Metallic Color']; }
+      if (v === 'PMSG') { return [v, ' Green Metallic Color']; }
+      if (v === 'PMSS') { return [v, ' San Simeon Silver Metallic Color']; }
+      if (v === 'PMTG') { return [v, ' Dolphin Grey Metallic Color']; }
+      if (v === 'PPMR') { return [v, ' Red Multi-Coat Color']; }
+      if (v === 'PPSB') { return [v, ' Deep Blue Metallic Color']; }
+      if (v === 'PPSR') { return [v, ' Signature Red Color']; }
+      if (v === 'PPSW') { return [v, ' Pearl White Multi-Coat Color']; }
+      if (v === 'PPTI') { return [v, ' Titanium Metallic Color']; }
+    }
+    return ["", ""];
+  }
+
+  private optionCodesToDriveEngine(codes: String[]): String[] {
+    for (var v of codes) {
+      if (v === 'DV2W') { return [v, 'Rear-Wheel Drive']; }
+      if (v === 'DV4W') { return [v, 'All-Wheel Drive']; }
+    }
+    return ["", ""];
+  }
+  private optionCodesToProductName(codes: String[]): String[] {
+    for (var v of codes) {
+      if (v === 'MT300') { return [v, "Standard Range Rear-Wheel Drive"]; }
+      if (v === 'MT301') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT302') { return [v, "Long Range Rear-Wheel Drive"]; }
+      if (v === 'MT303') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MT304') { return [v, "Long Range All-Wheel Drive Performance"]; }
+      if (v === 'MT305') { return [v, "Mid Range Rear-Wheel Drive"]; }
+      if (v === 'MT307') { return [v, "Mid Range Rear-Wheel Drive"]; }
+      if (v === 'MT308') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT309') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT310') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MT311') { return [v, "Long Range All-Wheel Drive Performance"]; }
+      if (v === 'MT314') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT315') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MT316') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MT317') { return [v, "Long Range All-Wheel Drive Performance"]; }
+      if (v === 'MT320') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT322') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT321') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MT323') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MT328') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MT336') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT337') { return [v, "Standard Range Plus Rear-Wheel Drive"]; }
+      if (v === 'MT340') { return [v, "Long Range All-Wheel Drive Performance"]; }
+      if (v === 'MTS01') { return [v, "Standard Range"]; }
+      if (v === 'MTS03') { return [v, "Long Range"]; }
+      if (v === 'MTS04') { return [v, "Performance"]; }
+      if (v === 'MTS05') { return [v, "Long Range"]; }
+      if (v === 'MTS06') { return [v, "Performance"]; }
+      if (v === 'MTS07') { return [v, "Long Range Plus"]; }
+      if (v === 'MTS08') { return [v, "Performance"]; }
+      if (v === 'MTS09') { return [v, "Plaid+"]; }
+      if (v === 'MTS10') { return [v, "Long Range"]; }
+      if (v === 'MTS11') { return [v, "Plaid"]; }
+      if (v === 'MTX01') { return [v, "Standard Range"]; }
+      if (v === 'MTX03') { return [v, "Long Range"]; }
+      if (v === 'MTX04') { return [v, "Performance"]; }
+      if (v === 'MTX05') { return [v, "Long Range Plus"]; }
+      if (v === 'MTX06') { return [v, "Performance"]; }
+      if (v === 'MTX07') { return [v, "Long Range Plus"]; }
+      if (v === 'MTX08') { return [v, "Performance"]; }
+      if (v === 'MTX10') { return [v, "Long Range"]; }
+      if (v === 'MTX11') { return [v, "Plaid"]; }
+      if (v === 'MTY01') { return [v, "Standard Range Rear-Wheel Drive"]; }
+      if (v === 'MTY02') { return [v, "Long Range Rear-Wheel Drive"]; }
+      if (v === 'MTY03') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MTY04') { return [v, "Long Range All-Wheel Drive Performance"]; }
+      if (v === 'MTY05') { return [v, "Long Range All-Wheel Drive Performance"]; }
+      if (v === 'MTY07') { return [v, "Long Range All-Wheel Drive"]; }
+      if (v === 'MTY09') { return [v, "Long Range All-Wheel Drive"]; }
+    }
+    return ["", ""];
+  }
+  private optionCodesToInterior(codes: String[]): String[] {
+    for (var v of codes) {
+      if (v === 'IN3BB') { return [v, "All Black Partial Premium Interior"]; }
+      if (v === 'IBW0') { return [v, "Black and White Interior"]; }
+      if (v === 'IBW1') { return [v, "Black and White Interior"]; }
+      if (v === 'IN3BW') { return [v, "Black and White Interior"]; }
+      if (v === 'IN3PB') { return [v, "All Black Premium Interior"]; }
+      if (v === 'IN3PW') { return [v, "All White Premium Interior"]; }
+      if (v === 'IBE00') { return [v, "Wood Décor & Black Interior"]; }
+      if (v === 'ICW00') { return [v, "Wood Décor & Cream Interior"]; }
+      if (v === 'IWW00') { return [v, "Wood Décor & Black and White Interior"]; }
+      if (v === 'IBC00') { return [v, "Carbon Fiber Décor & Black Interior"]; }
+      if (v === 'IWC00') { return [v, "Carbon Fiber Décor & Black and White Interior"]; }
+      if (v === 'ICC00') { return [v, "Carbon Fiber Décor & Cream Interior"]; }
+      if (v === 'INBBW') { return [v, "White Interior"]; }
+      if (v === 'INB3C') { return [v, "Premium beige interior with oak wood finishes"]; }
+      if (v === 'INBC3W') { return [v, "Premium black and white interior with Carbon Fiber decor"]; }
+      if (v === 'INPB0') { return [v, "All Black Interior with Wood in door panel"]; }
+      if (v === 'INPB1') { return [v, "All Black Interior"]; }
+      if (v === 'INPW0') { return [v, "Black and White Interior with Wood in door panel"]; }
+      if (v === 'INPW1') { return [v, "Black and White Interior"]; }
+      if (v === 'INBFP') { return [v, "Classic Black Interior"]; }
+      if (v === 'INBPP') { return [v, "Black Interior"]; }
+      if (v === 'INBPW') { return [v, "White Seats Interior"]; }
+      if (v === 'INBTB') { return [v, "Multi-Pattern Black Interior"]; }
+      if (v === 'INFBP') { return [v, "Black Premium Interior"]; }
+      if (v === 'INLPC') { return [v, "Cream Interior"]; }
+      if (v === 'INLPP') { return [v, "Black / Light Headliner Interior"]; }
+      if (v === 'INWPT') { return [v, "Tan Interior"]; }
+      if (v === 'INYPB') { return [v, "All Black Premium Interior"]; }
+      if (v === 'INYPW') { return [v, "Black and White Premium Interior"]; }
+      if (v === 'IPB0') { return [v, "Black Interior"]; }
+      if (v === 'IPB1') { return [v, "Black Interior"]; }
+      if (v === 'IPW0') { return [v, "White Interior"]; }
+      if (v === 'IPW1') { return [v, "white Interior"]; }
+      if (v === 'IVBPP') { return [v, "All Black Interior"]; }
+      if (v === 'IVBSW') { return [v, "Ultra White Interior"]; }
+      if (v === 'IVBTB') { return [v, "All Black Interior"]; }
+      if (v === 'IVLPC') { return [v, "Vegan Cream Interior"]; }
+    }
+    return ["", ""];
+  }
+  private optionCodesToSpoiler(codes: String[]): String[] {
+    for (var v of codes) {
+      if (v === 'SLR0') { return [v, 'No Rear Spoiler']; }
+      if (v === 'SLR1') { return [v, 'Carbon Fibre Spoiler']; }
+    }
+    return ["", ""];
+  }
+  private async getVehicleInfo(vid: string, vehicle: tjs.Vehicle): Promise<any> {
     let option: tjs.optionsType = {
       authToken: this.token || '',
       vehicleID: vid,
     };
 
-    let opcodes = vehicle.option_codes as string;
-    let model = '';
+    let opcodes = vehicle.option_codes as String;
+    let codes = opcodes.split(',');
+    let model = this.optionCodesToCarType(codes);
+    let wheel = this.optionCodesToWheelType(codes);
+    let color = this.optionCodesToPaintColor(codes);
+    let engine = this.optionCodesToDriveEngine(codes);
+    let interior = this.optionCodesToInterior(codes);
+    let subtype = this.optionCodesToProductName(codes);
+    let spoiler = this.optionCodesToSpoiler(codes);
     let view = 'STUD_SIDE';
-    let cs = opcodes.split(',').filter((v, i, arr) => {
-      if (v === 'MDLS' || v === 'MS03' || v === 'MS04') {
-        model = 'ms';
-      }
-      if (v === 'MDLX') {
-        model = 'mx';
-      }
-      if (v === 'MDL3') {
-        model = 'm3';
-      }
-      if (v === 'MDLY') {
-        model = 'my';
-        view = 'SIDE';
-      }
-      return v.startsWith('W') || v.match(/PP[A-Z]*/) || v.startsWith('SLR') || v.startsWith('DV') || v.startsWith('IN') || v.startsWith('MT') || v.startsWith('MI');
-    }).map((v, i, arr) => {
-      return `$${v}`;
-    }).join(',');
-    const image = `https://static-assets.tesla.com/configurator/compositor?&options=${cs}&view=STUD_SIDE&model=${model}&size=400&bkba_opt=1`;
+    if (model[0] === 'my') {
+      view = 'STUD_3QTR';
+    }
+    let cs = `${subtype[0]},${color[0]},${wheel[0]},${spoiler},${interior[0]}`;
+    const image = `https://static-assets.tesla.com/configurator/compositor?&options=${cs}&view=${view}&model=${model[0]}&size=400&bkba_opt=1`;
 
     if (vehicle.state === 'asleep') {
-      return new Promise<Object>((resolve, reject) => {
-        let vd = { vehicle_config: { car_type: tjs.getModel(vehicle) }, image };
+      return new Promise<any>((resolve, reject) => {
+        let vd = { image };
         resolve(Object.assign(vd, vehicle));
       });
     } else {
-      return tjs.vehicleDataAsync(option).then(v => {
-        let vd = { image };
+      return tjs.vehicleDataAsync(option).then(result => {
+        let v = result as any;
+        let location = `https://www.google.com/maps/search/?api=1&query=${v.drive_state.corrected_latitude},${v.drive_state.corrected_longitude}`;
+        if (codes.includes('COCN')) {
+          location = `https://uri.amap.com/marker?position=${v.drive_state.corrected_longitude},${v.drive_state.corrected_latitude}&name=${v.display_name}`;
+        }
+        let vd = { image, location };
         let vv = Object.assign(vd, v);
         return vv;
       });
